c3394667ae48e5115e0932e7e3eef883ffcc1f8b
vodoleev-vladislav <vodoleev.vladislav@gmail.com>
2020-08-04 15:35:27 +0300

    add few small fixes

--- src/components/Table.js
+++ src/components/Table.js
@@ -25,41 +25,39 @@ const MainTable = ({
   };
 
   return (
-    <>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell key={column} onClick={handleTableHeadClick(column)}>
-                  <b>{column}</b>
-                  {column === sortParams.field
-                    ? sortParams.order === "asc"
-                      ? String.fromCharCode(8593)
-                      : String.fromCharCode(8595)
-                    : null}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {entries.map((entry) => (
-              <TableRow
-                key={entry.firstName + entry.lastName + entry.id}
-                onClick={(e) => handleRowClick(e, entry)}
-                className={selectedRow === entry ? "table-row--selected" : ""}
-              >
-                {Object.keys(entry).map((column) =>
-                  columns.includes(column) ? (
-                    <TableCell key={column}>{entry[column]}</TableCell>
-                  ) : null
-                )}
-              </TableRow>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell key={column} onClick={handleTableHeadClick(column)}>
+                <b>{column}</b>
+                {column === sortParams.field
+                  ? sortParams.order === "asc"
+                    ? String.fromCharCode(8593)
+                    : String.fromCharCode(8595)
+                  : null}
+              </TableCell>
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {entries.map((entry) => (
+            <TableRow
+              key={entry.firstName + entry.lastName + entry.id}
+              onClick={(e) => handleRowClick(e, entry)}
+              className={selectedRow === entry ? "table-row--selected" : ""}
+            >
+              {Object.keys(entry).map((column) =>
+                columns.includes(column) ? (
+                  <TableCell key={column}>{entry[column]}</TableCell>
+                ) : null
+              )}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
